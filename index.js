e24a576bb9353108722d09ed3d9f34701a3c721d
Akshay Sood <akshaysood@icloud.com>
2019-04-19 11:54:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { TransactionProcessor } = require('sawtooth-sdk/processor')
 const SimpleStoreHandler = require('./handler')
 const transactionProcessor = new TransactionProcessor("tcp://localhost:4004")
 
-transactionProcessor.addHandler(new SimpleStoreHandler())
+
 transactionProcessor.start()
 
 console.log(`Starting bank transaction processor`)
